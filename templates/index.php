90b289b776b8a3bb6f1e99564af99fe7df92d7f6
Priyanka Menghani <priyanka.menghani@gmail.com>
2012-09-30 22:33:51 +0530

    PDFs open with integrated PDF reader.

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,7 @@
 <div id = "controls">
 	<!-- TODO: add controls -->
 	<div id="file_action_panel"></div>
-	<!-- Set dir value to be passed to the showPDFviewer function -->
+	<!-- Set dir value to be passed to integrate.js -->
 	<input type="hidden" name="dir" value="<?php echo empty($_['dir'])?'':rtrim($_['dir'],'/') ?>" id="dir">
 </div>
 <div class = "actions"></div>
@@ -25,33 +25,25 @@
 		$file['extension'] = '.'.$file_info['extension'];	
 		$files[] = $file;
 	}
-
-	/* Display pdf files in the current directory and links to sub-directories 
-	* inside the current directory which contain pdf files. */
 ?>
 
-<div id='notification'></div>
-
 <table>
-	<thead>
-		LIST
-	</thead>
 	<tbody id = "fileList">
 		<?php
 		// Array to store directory entries, which contain pdfs.
 			$dirs = array();
 			foreach ($files as $file) {
-				// Encode the file and directory names.
+				// Encode the file and directory names so that they can be used in querying a url.
 				$name = str_replace('+','%20',urlencode($file['filename']));
 				$name = str_replace('%2F','/', $name);
 				$directory = str_replace('+','%20',urlencode($dir));
 				$directory = str_replace('%2F','/', $directory);
 				if ($file['dirname'] == '.') { 
-		?>			<!-- Each tr displays a file -->	
-					<tr data-file="<?php echo $name;?>" data-type="<?php echo 'file'?>" data-mime="<?php echo 'application/pdf'?>">
+		?>			
+					<!-- Each tr displays a file -->	
+					<tr data-file="<?php echo $name;?>" data-type="<?php echo 'file'?>" data-mime="<?php echo 'application/pdf'?>" data-size="3462755" data-write="true" >
 						<td class="filename svg" style="background-image:url(<?php echo OCP\mimetype_icon('application/pdf'); ?>)">
-							<!-- link passes to files/download.php that renders contents of the file -->
-							<a class="name" href="<?php echo \OCP\Util::linkTo('files', 'download.php').'?file='.$directory.$name; ?>" title="">
+							<a class="name" href="http://localhost<?php echo \OCP\Util::linkTo('files', 'download.php').'?file='.$directory.$name; ?>" title="">
 								<span class = "nametext">
 									<?php echo htmlspecialchars($file['basename']);?><span class='extension'><?php echo $file['extension'];?></span>
 								</span>
@@ -67,7 +59,8 @@
 					// Explode the variable to check if the pdf file is contained in a directory.
 					$dir_array = explode('/', $dir_name);
 					// Each tr entry here contains name of a directory which has a link to reader/index.php.
-					$d = '<tr data-file="'.$file['filename'].'" data-type="dir" data-mime="dir">
+					// TODO: correct mime type to be rendered explicitly.
+					$d = '<tr data-file="'.$dir_array[0].'" data-type="dir" data-mime="httpd/unix-directory">
 							<td class="filename svg" style="background-image:url('.OCP\mimetype_icon('dir').')">
 								<a class = "name" href = "'.OCP\Util::linkTo('reader', 'index.php').'&dir='.$dir.$dir_array[0].'/">
 									<span class = "nametext">'.
@@ -76,17 +69,17 @@
 								</a>
 							</td>
 						</tr>';
-						/* Store the directory entries in an array so that incase 2 pdf files are conatined in a directory
-						 * we don't end up printing the directory name twice. */
-						if (!in_array($d, $dirs)) {
-							$dirs[] = $d;
-							echo $d;
-							echo '<br>';
-						}
+					/* Store the directory entries in an array so that incase 2 pdf files are conatined in a directory
+					 * we don't end up printing the directory name twice. */
+					if (!in_array($d, $dirs)) {
+						$dirs[] = $d;
+						echo $d;
+						echo '<br>';
+					}
 				}
 			}
 		?>
 	</tbody>
 </table>	
-		
-<div id = "editor"></div>
+
+
